af2813368d095a66f2757dec4f9b377d548c9673
Mohamed Magdy <moh.magdy40@gmail.com>
2024-02-02 15:04:17 +0200

    [ADD] Add a new module: base_bank_from_iban

--- base_bank_from_iban/__manifest__.py
+++ base_bank_from_iban/__manifest__.py
@@ -1,10 +1,7 @@
-# Copyright 2017 Tecnativa - Carlos Dauden
-# Copyright 2018-2022 Tecnativa - Pedro M. Baeza
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3).
 
 {
     "name": "Bank from IBAN",
-    "version": "16.0.2",
+    "version": "1.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/community-data-files",
     "category": "Localization",
