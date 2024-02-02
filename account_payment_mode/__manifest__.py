ce9fc4f38f97e6d11cb049a3d2e72374d57234a4
Mohamed Magdy <moh.magdy40@gmail.com>
2024-02-02 15:00:59 +0200

    [ADD] Add upgraded modules: account_due_list and account_payment_mode.

--- account_payment_mode/__manifest__.py
+++ account_payment_mode/__manifest__.py
@@ -1,16 +1,12 @@
-# Copyright 2016-2020 Akretion France (<https://www.akretion.com>)
-# @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account Payment Mode",
-    "version": "16.0.1",
-    "development_status": "Mature",
-    "license": "AGPL-3",
+    'version': '1.0',
     "author": "Akretion,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/bank-payment",
     "category": "Banking addons",
-    "depends": ["account"],
+    "license": "LGPL-3",
+    "depends": ["base","account","account_accountant"],
     "data": [
         "security/account_payment_mode.xml",
         "security/ir.model.access.csv",
@@ -18,6 +14,11 @@
         "views/account_payment_mode.xml",
         "views/account_journal.xml",
     ],
-    "demo": ["demo/payment_demo.xml"],
+    "demo": [
+
+        "demo/payment_demo.xml"
+
+    ],
     "installable": True,
+    "auto_install": False,
 }
