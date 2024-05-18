3f888e2256a38794c0bb89b66109db2be4b11c0f
ms-salah <mohamed.salah@projomania.com>
2024-05-18 04:56:15 +0300

    [FIX]Payment

--- esco_account_report/models/account_payment.py
+++ esco_account_report/models/account_payment.py
@@ -116,27 +116,13 @@ class AccountPaymentRegister(models.TransientModel):
         return discount
 
     def _create_payment_vals_from_wizard(self, batch_result):
-        payment_vals = {
-            'date': self.payment_date,
-            'amount': self.amount,
-            'payment_type': self.payment_type,
-            'partner_type': self.partner_type,
-            'ref': self.communication,
-            'journal_id': self.journal_id.id,
-            'currency_id': self.currency_id.id,
-            'payment_difference_handling': self.payment_difference_handling,
-            'discount_amount': self.discount_amount,
-            'partner_id': self.partner_id.id,
-            'writeoff_label': self.writeoff_label,
-            'partner_bank_id': self.partner_bank_id.id,
-            'payment_method_line_id': self.payment_method_line_id.id,
-            'destination_account_id': self.line_ids[0].account_id.id
-        }
+        payment_vals = super()._create_payment_vals_from_wizard(batch_result)
 
         if not self.currency_id.is_zero(self.payment_difference) and self.payment_difference_handling == 'reconcile':
             payment_vals['write_off_line_vals'] = {
                 'name': self.writeoff_label,
                 'amount': self.payment_difference,
+                'currency_id': self.currency_id.id,
                 'account_id': self.writeoff_account_id.id,
             }
 
@@ -144,6 +130,7 @@ class AccountPaymentRegister(models.TransientModel):
             payment_vals['write_off_line_vals'] = {
                 'name': self.writeoff_label,
                 'amount': self.discount_amount,
+                'currency_id': self.currency_id.id,
                 'account_id': self.writeoff_account_id.id,
             }
         return payment_vals
