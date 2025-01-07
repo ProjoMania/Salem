d5fe125a5e860ccfb884ccef07f4324857964e53
Mohamed Magdy <moh.magdy40@gmail.com>
2025-01-08 00:34:15 +0200

    [FIX] Force the user to select on of the checkboxes.

--- vendor_sales_report_report_date/models/sale_order.py
+++ vendor_sales_report_report_date/models/sale_order.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields,api
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
@@ -10,6 +11,13 @@ class SaleOrder(models.Model):
     is_report_date = fields.Boolean(string="Report Date" ,defualt=False)
     is_report_date1_invisible = fields.Boolean('Report Date',compute='_compute_is_report_date1_invisible')
     is_match_inv_date = fields.Boolean(string="Match Invoice date" ,defualt=False)
+    
+    @api.constrains('is_match_inv_date','is_report_date')
+    def _check_is_match_inv_date(self):
+        if self.is_match_inv_date and self.is_report_date:
+            raise ValidationError("You can't select both Match Invoice Date and Report Date")
+        if not self.is_match_inv_date and not self.is_report_date:
+            raise ValidationError("You must select either Match Invoice Date or Report Date")
 
     @api.depends('invoice_ids','is_match_inv_date','is_report_date')
     def _compute_report_date(self):
