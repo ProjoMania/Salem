f886d79ce436d316622ee71d4c9b63499c8ca5af
Mohamed Magdy <moh.magdy40@gmail.com>
2025-01-07 22:51:00 +0200

    [FIX] Fix the relation.

--- vendor_sales_report_report_date/models/account_move.py
+++ vendor_sales_report_report_date/models/account_move.py
@@ -14,6 +14,8 @@ class AccountMove(models.Model):
         for rec in self:
             if rec.move_type == 'out_invoice':
                 rec.report_date = rec.line_ids.sale_line_ids.order_id.report_date
+            elif rec.move_type == 'out_refund':
+                rec.report_date = rec.invoice_date
             else:
                 rec.report_date = False
 
