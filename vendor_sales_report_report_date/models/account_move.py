8c844fc169132aa5c2a648432ded58fe3d599e7e
Mohamed Magdy <moh.magdy40@gmail.com>
2025-01-08 22:14:06 +0200

    [FIX] Fix the dates in the generated excel sheet.

--- vendor_sales_report_report_date/models/account_move.py
+++ vendor_sales_report_report_date/models/account_move.py
@@ -12,15 +12,11 @@ class AccountMove(models.Model):
     @api.depends('line_ids')
     def _compute_report_date(self):
         for rec in self:
-            if rec.move_type == 'out_invoice':
-                if rec.line_ids.sale_line_ids and rec.line_ids.sale_line_ids.order_id.report_date:
-                    rec.report_date = rec.line_ids.sale_line_ids.order_id.report_date
-                else:
-                    rec.report_date = rec.invoice_date
-            elif rec.move_type == 'out_refund':
-                rec.report_date = rec.invoice_date
+            if rec.move_type == 'out_invoice' and rec.line_ids.sale_line_ids:
+                order = rec.line_ids.sale_line_ids.order_id
+                rec.report_date = order.report_date if not order.is_match_inv_date else rec.invoice_date
             else:
-                rec.report_date = False
+                rec.report_date = rec.invoice_date if rec.move_type == 'out_refund' else False
 
 
 class AccountMoveLine(models.Model):
