765e8d79f936244299bcf3d212b8e5c0ebe9f769
Mohamed Magdy <moh.magdy40@gmail.com>
2025-06-17 01:45:45 +0300

    refactor: simplify company_id handling in product models by using default_get method

--- product_default_company/models/product_template.py
+++ product_default_company/models/product_template.py
@@ -4,27 +4,11 @@ from odoo.exceptions import ValidationError
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    company_id = fields.Many2one(
-        'res.company',
-        string='Company',
-        default=lambda self: self.env.company,
-        required=True,
-        index=True,
-    )
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            if not vals.get('company_id'):
-                vals['company_id'] = self.env.company.id
-        return super().create(vals_list)
-
-    def write(self, vals):
-        if 'company_id' not in vals:
-            for rec in self:
-                if not rec.company_id:
-                    vals['company_id'] = self.env.company.id
-        return super().write(vals)
+    @api.model
+    def default_get(self, fields):
+        res = super(ProductTemplate, self).default_get(fields)
+        res['company_id'] = self.env.company.id
+        return res
 
     @api.constrains('company_id')
     def _check_company_id(self):
