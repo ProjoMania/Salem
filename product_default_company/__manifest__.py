304de61c3652b49fe179893a95483e7b519e70c9
Mohamed Magdy <moh.magdy40@gmail.com>
2025-06-16 10:33:17 +0300

    feat: add product default company functionality to enforce company_id presence

---
+++ product_default_company/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Product Default Company',
+    'version': '17.0.1.0.0',
+    'summary': 'Set default company for products and enforce company_id presence',
+    'category': 'Product',
+    'author': 'Your Company',
+    'website': 'https://yourcompany.com',
+    'depends': ['product'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+} 
---
+++ product_default_company/models/product_product.py
@@ -0,0 +1,33 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    company_id = fields.Many2one(
+        'res.company',
+        string='Company',
+        default=lambda self: self.env.company,
+        required=True,
+        index=True,
+    )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if not vals.get('company_id'):
+                vals['company_id'] = self.env.company.id
+        return super().create(vals_list)
+
+    def write(self, vals):
+        if 'company_id' not in vals:
+            for rec in self:
+                if not rec.company_id:
+                    vals['company_id'] = self.env.company.id
+        return super().write(vals)
+
+    @api.constrains('company_id')
+    def _check_company_id(self):
+        for rec in self:
+            if not rec.company_id:
+                raise ValidationError(_('The company must be set for the product.')) 
